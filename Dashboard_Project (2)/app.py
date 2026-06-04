dc8b9cdc9eb4e9f9ac7a7ac4af35812aeac1716b
Ubaid Ullah <70177858@student.uol.edu.pk>
2026-06-04 10:33:43 +0500

    Updated

--- Dashboard_Project (2)/app.py
+++ Dashboard_Project (2)/app.py
@@ -158,11 +158,14 @@ st.markdown("""
 # =============================================================================
 # LOAD DATA
 # =============================================================================
+
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/trees.csv")
-    df['created_at'] = pd.to_datetime(df['created_at'], format='%m/%d/%Y', errors='coerce')
+    url = "https://data.cityofnewyork.us/api/views/uvpi-gqnh/rows.csv?accessType=DOWNLOAD"
+    df = pd.read_csv(url)
     return df
+
+df = load_data()
 
 try:
     df = load_data()
